83fcf48780df8c42699f11788a3e26da525e5d19
talarczyk <wojtek.talarczyk@gmail.com>
2016-06-20 10:21:10 +0200

    change config
    password change

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+$config = [
+    'cms.debug' => 1,
+    'cms.title' => 'My custom CMS',
+    'cms.description' => 'Project created to show how CMS works',
+    'cookie.lifetime' => 7200,
+    'db.name' => 'kolegait_21183',
+    'db.host' => 'sql.s24.vdl.pl',
+    'db.user' => 'kolegait_21183',
+    'db.password' => '21183',
+    'session.lifetime' => 7200
+];
+
